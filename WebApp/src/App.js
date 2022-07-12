9391619a52f05969683fde112d83471740560906
Igor Crevar <crewce@gmail.com>
2022-07-12 19:22:10 +0200

    change route fix

--- WebApp/src/App.js
+++ WebApp/src/App.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useDispatch } from 'react-redux'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
 import AppRoutes from './routes/AppRoutes'
@@ -11,11 +11,10 @@ import { loadAllSubjectsAction } from './actions/subjectActions'
 
 function App() {
     const dispatch = useDispatch()
-    const location = useLocation()
     const navigate = useNavigate()
 
     const initCallback = useCallback(async () => {
-        await initializeEthAction(location, navigate, dispatch)
+        await initializeEthAction(navigate, dispatch)
         await loadUsersAction(dispatch)
         await loadAllSubjectsAction(dispatch)
         // eslint-disable-next-line react-hooks/exhaustive-deps
--- WebApp/src/actions/appActions.js
+++ WebApp/src/actions/appActions.js
@@ -2,17 +2,18 @@ import { setSelectedAccount, setGasPrice } from '../state/ethReducer'
 import web3 from '../web3'
 import EventListenerService from "../utils/eventListenerService"
 
-export const initializeEthAction = async (location, navigate, dispatch) => {
+export const initializeEthAction = async (navigate, dispatch) => {
     window.ethereum.on('accountsChanged', async _ => {
         // Handle the new accounts, or lack thereof.
         // "accounts" will always be an array, but it can be empty.
 
         // accounts param returns all lowercase, so we fetch like this
         const accounts = await web3.eth.getAccounts();
-        if (location && location.pathname !== '/' && location.pathname !== '') {
-            navigate('/')
-        }
         dispatch(setSelectedAccount(accounts.length > 0 ? accounts[0] : undefined))
+        const pathname = window.location ? window.location.pathname  : ''
+        if (pathname !== '/' && pathname !== '') {
+            navigate('/')
+        }        
     })
 
     try {
