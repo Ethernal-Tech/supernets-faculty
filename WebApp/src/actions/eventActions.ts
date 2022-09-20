477ddc86dad02fbedfdfd01b58920931dd678282
mika <miodragmarcic@yahoo.com>
2022-09-20 23:25:24 +0200

    LoadEvent implemented

--- WebApp/src/actions/eventActions.ts
+++ WebApp/src/actions/eventActions.ts
@@ -2,67 +2,6 @@ import faculty from '../faculty'
 import reader from '../facultyReader'
 import EventListenerService from "utils/eventListenerService"
 
-export const getEventAction = () => {
-	const event: any = [
-		"1",
-		"Event1",
-		"NS",
-		"FTN",
-		"1662681600000",
-		"1663200000000",
-		" Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description",
-		[
-			"0x1d5389A562228203FcB23F19E9eF04385b676c26"
-		],
-		[
-			"0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",
-			"0x5885314C3A4eb71Ad5ADDf4c767BBa890fd66De6",
-			"0x1aE0EA34a72D944a8C7603FfB3eC30a6669E454C",
-			"0x03C6FcED478cBbC9a4FAB34eF9f40767739D1Ff7"
-		],
-		[
-			"0xcB4ba64C6e205451e795e7f5831EA9E56436c0d0",
-			"0x5c6B0f7Bf3E7ce046039Bd8FABdfD3f9F5021678",
-			"0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C",
-			"0x583031D1113aD414F02576BD6afaBfb302140225",
-			"0x0A098Eda01Ce92ff4A4CCb7A4fFFb5A43EBC70DC",
-			"0xdD870fA1b7C4700F2BD7f44238821C26f7392148",
-			"0xAE7Dd4464a9567234Ec328AE8fA3a8Bb1BaE6383"
-		],
-		[
-			"1",
-			"2",
-			"6",
-			"13",
-			"14"
-		],
-		true
-	];
-
-	(event as any).adminsAddresses = [
-		"0x1d5389A562228203FcB23F19E9eF04385b676c26"
-	];
-	(event as any).coursesIds = [
-		"1",
-		"2",
-		"6",
-		"13",
-		"14"
-	];
-	(event as any).description = " Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description";
-	(event as any).endDate = "1663200000000";
-	(event as any).exist = true;
-	(event as any).id ="1";
-	(event as any).location = "NS";
-	(event as any).professorsAddresses = ['0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db', '0x5885314C3A4eb71Ad5ADDf4c767BBa890fd66De6', '0x1aE0EA34a72D944a8C7603FfB3eC30a6669E454C', '0x03C6FcED478cBbC9a4FAB34eF9f40767739D1Ff7'];
-	(event as any).startDate = "1662681600000";
-	(event as any).studentsAddresses = ['0xcB4ba64C6e205451e795e7f5831EA9E56436c0d0', '0x5c6B0f7Bf3E7ce046039Bd8FABdfD3f9F5021678', '0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C', '0x583031D1113aD414F02576BD6afaBfb302140225', '0x0A098Eda01Ce92ff4A4CCb7A4fFFb5A43EBC70DC', '0xdD870fA1b7C4700F2BD7f44238821C26f7392148', '0xAE7Dd4464a9567234Ec328AE8fA3a8Bb1BaE6383'];
-	(event as any).title = "Event1";
-	(event as any).venue = "FTN";
-
-	return event
-}
-
 export const addEventAction = async (title, location, venue, startDate, endDate, description, account, dispatch) => {
     try {
         await faculty.methods.addEditEvent(0, title, location, venue, startDate, endDate, description).send({ from: account });
--- WebApp/src/containers/Event/EventRouter.tsx
+++ WebApp/src/containers/Event/EventRouter.tsx
@@ -1,39 +1,41 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { Switch, Route, useRouteMatch } from 'react-router-dom'
+import { Switch, Route, useRouteMatch, useParams } from 'react-router-dom'
 import { getUserRole } from 'utils/userUtils'
 import { Details } from './Details/Details'
 import { USER_ROLES } from 'utils/constants';
 import { AdminEventNavbar } from './AdminEventNavbar';
 import { ProfessorEventNavbar } from './ProfessorEventNavbar';
 import { StudentEventNavbar } from './StudentEventNavbar';
-import { getEventAction } from 'actions/eventActions';
 import { ProfessorsRouter } from '../Professors/ProfessorsRouter';
 import { EventCourses } from './EventCourses/EventCourses';
 import { StudentsRouter } from 'containers/Students/StudentsRouter';
 import { Admins } from './Admins/Admins';
 import { loadUsersAction } from 'actions/userActions';
 import { loadAllCoursesAction } from 'actions/coursesActions';
-import { useCallback, useEffect, useMemo } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Courses } from 'containers/Courses/Courses';
+import { loadEvent } from 'actions/eventActions';
+import { ClipSpinner, Spinner } from 'components/Spinner';
 
 export const EventRouter = () => {
 	const dispatch = useDispatch()
 	const { path } = useRouteMatch()
+	const params: any = useParams()
+	const eventId = params.eventId
+
 	const state = useSelector((state: any) => state)
 	const userRole = getUserRole(state)
 
-	// FIXME: get event, users and courses stavi zajedno unutar Promise.all i dodaj neki spinner
-	const event = useMemo(
-		() => getEventAction(),
-		[]
-	)
+	const [event, setEvent] = useState<any>()
 
 	const loadData = useCallback(
 		async () => {
-			await loadUsersAction(event.id, dispatch)
-			await loadAllCoursesAction(event.id, dispatch)
+			const loadedEvent = await loadEvent(eventId)
+			await loadUsersAction(eventId, dispatch)
+			await loadAllCoursesAction(eventId, dispatch)
+			setEvent(loadedEvent)
 		},
-		[dispatch, event.id]
+		[dispatch, eventId]
 	)
 
 	useEffect(
@@ -43,12 +45,20 @@ export const EventRouter = () => {
 		[loadData]
 	)
 
+	if (!event) {
+		return (
+			<Spinner>
+				<ClipSpinner size={80} />
+			</Spinner>
+		)
+	}
+
 	return (
 		<div style={{ display: 'flex', height: 'calc(100% - 60px)' }}>
 			<div style={{ width: '250px', padding: '24px', background: '#4d4d4d' }}>
 				{userRole === USER_ROLES.ADMIN && <AdminEventNavbar event={event} />}
-				{userRole === USER_ROLES.PROFESSOR && <ProfessorEventNavbar />}
-				{userRole === USER_ROLES.STUDENT && <StudentEventNavbar />}
+				{userRole === USER_ROLES.PROFESSOR && <ProfessorEventNavbar event={event} />}
+				{userRole === USER_ROLES.STUDENT && <StudentEventNavbar event={event} />}
 			</div>
 			<div style={{ flex: 1, maxWidth: '1280px' }}>
 				<Switch>
