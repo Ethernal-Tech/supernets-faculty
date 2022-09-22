716f8a0fba4ecc2a17d2a4175230189bf933bb77
mika <miodragmarcic@yahoo.com>
2022-09-22 22:19:15 +0200

    Export RootState from store

--- WebApp/src/state/store.ts
+++ WebApp/src/state/store.ts
@@ -4,11 +4,15 @@ import coursesReducer from './coursesReducer'
 import usersReducer from './usersReducer'
 import certificatesReducer from './certificatesReducer'
 
-export default configureStore({
-    reducer: {
-        eth: ethReducer,
-        users: usersReducer,
-        courses: coursesReducer,
-        certificates: certificatesReducer
-    },
+const store = configureStore({
+	reducer: {
+		eth: ethReducer,
+		users: usersReducer,
+		courses: coursesReducer,
+		certificates: certificatesReducer
+	}
 })
+
+export default store
+
+export type RootState = ReturnType<typeof store.getState>
